c57c9b343000fb15bde0fbfd250b73fa1d975684
willys <w1llys_910710@yahoo.co.id>
2021-02-12 22:37:38 +0700

    CRUD city finish

--- resources/views/city/index.blade.php
+++ resources/views/city/index.blade.php
@@ -19,7 +19,9 @@
                         <td class="text-center font-size-sm">{{$x}}</td>
                         <td class="d-sm-table-cell font-size-sm">{{ucwords($c->province->name)}}</td>
                         <td class="d-sm-table-cell font-size-sm">{{$c->name}}</td>
-                        <td></td>
+                        <td class="text-center">
+                            <a onClick='showM("{{url('city/'.$c->id.'/edit')}}");return false' type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Edit"><i class="fas fa-fw fa-pencil-alt"></i></a>
+                        </td>
                     </tr>   
                     @php $x++ @endphp
                     @endforeach
@@ -31,25 +33,17 @@
 @endsection
 
 @section ('headerScript')
-@livewireStyles
 <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/dataTables.bootstrap4.css')}}">
 <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/responsive.bootstrap4.min.css')}}">
-<link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/buttons-bs4/buttons.bootstrap4.min.css')}}">
 <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/select2/css/select2.min.css')}}">
 <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/flatpickr/flatpickr.min.css')}}">
 @endsection
 
 @section ('footerScript')
-@livewireScripts
-<script> Livewire.restart();</script>
 <script src="{{asset('public/js/plugins/datatables/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('public/js/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/datatables/buttons/dataTables.buttons.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/datatables/buttons/buttons.print.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/datatables/buttons/buttons.html5.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/datatables/buttons/buttons.flash.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/datatables/buttons/buttons.colVis.min.js')}}"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.6/js/responsive.bootstrap4.min.js"></script>
+<script src="{{asset('public/js/siapfulin.datatable.js')}}"></script>
 <script src="{{asset('public/js/siapfulin.js')}}"></script>
 @endsection
